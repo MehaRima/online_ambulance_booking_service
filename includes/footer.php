824d55d5f13949b4b30ace864fd045f2eddbce35
SAHELI  BASU <26185528+MehaRima@users.noreply.github.com>
2020-07-13 20:19:24 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,87 @@
+<?php
+if(isset($_POST['emailsubscibe']))
+{
+$subscriberemail=$_POST['subscriberemail'];
+$sql ="SELECT SubscriberEmail FROM tblsubscribers WHERE SubscriberEmail=:subscriberemail";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':subscriberemail', $subscriberemail, PDO::PARAM_STR);
+$query-> execute();
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query -> rowCount() > 0)
+{
+echo "<script>alert('Already Subscribed.');</script>";
+}
+else{
+$sql="INSERT INTO  tblsubscribers(SubscriberEmail) VALUES(:subscriberemail)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':subscriberemail',$subscriberemail,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+echo "<script>alert('Subscribed successfully.');</script>";
+}
+else 
+{
+echo "<script>alert('Something went wrong. Please try again');</script>";
+}
+}
+}
+?>
+
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+      
+        <div class="col-md-6">
+          <h6>About Us</h6>
+          <ul>
+
+        
+          <li><a href="page.php?type=aboutus">About Us</a></li>
+            <li><a href="page.php?type=faqs">FAQs</a></li>
+            <li><a href="page.php?type=privacy">Privacy</a></li>
+          <li><a href="page.php?type=terms">Terms of use</a></li>
+               <li><a href="admin/">Admin Login</a></li>
+          </ul>
+        </div>
+  
+        <div class="col-md-3 col-sm-6">
+          <h6>Subscribe Newsletter</h6>
+          <div class="newsletter-form">
+            <form method="post">
+              <div class="form-group">
+                <input type="email" name="subscriberemail" class="form-control newsletter-input" required placeholder="Enter Email Address" />
+              </div>
+              <button type="submit" name="emailsubscibe" class="btn btn-block">Subscribe <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></button>
+            </form>
+            <p class="subscribed-text">*We send great deals and latest auto news to our subscribed users very week.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6 text-right">
+          <div class="footer_widget">
+            <p>Connect with Us:</p>
+            <ul>
+              <li><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+              <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-6 col-md-pull-6">
+          <p class="copy-right">Copyright &copy; 2020 Aarogya Service Portal. All Rights Reserved</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,109 @@
+
+<header>
+  <div class="default-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-md-2">
+          <div class="logo"> <a href="index.php"><img src="assets/images/logo.png" alt="image"/></a> </div>
+        </div>
+        <div class="col-sm-9 col-md-10">
+          <div class="header_info">
+            <div class="header_widgets">
+              <div class="circle_icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </div>
+              <p class="uppercase_text">For Support Mail us : </p>
+              <a href="mailto:info@example.com">info@example.com</a> </div>
+            <div class="header_widgets">
+              <div class="circle_icon"> <i class="fa fa-phone" aria-hidden="true"></i> </div>
+              <p class="uppercase_text">Service Helpline Call Us: </p>
+              <a href="tel:61-1234-5678-09">1800-5050-9999</a> </div>
+            <div class="social-follow">
+              <ul>
+                <li><a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              </ul>
+            </div>
+   <?php   if(strlen($_SESSION['login'])==0)
+	{	
+?>
+ <div class="login_btn"> <a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> </div>
+<?php }
+else{ 
+
+echo "Welcome To The Portal";
+ } ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Navigation -->
+  <nav id="navigation_bar" class="navbar navbar-default">
+    <div class="container">
+      <div class="navbar-header">
+        <button id="menu_slide" data-target="#navigation" aria-expanded="false" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      </div>
+      <div class="header_wrap">
+        <div class="user_login">
+          <ul>
+            <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i> 
+<?php 
+$email=$_SESSION['login'];
+$sql ="SELECT FullName FROM tblusers WHERE EmailId=:email ";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+	{
+
+	 echo htmlentities($result->FullName); }}?><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+              <ul class="dropdown-menu">
+           <?php if($_SESSION['login']){?>
+            <li><a href="profile.php">Profile Settings</a></li>
+              <li><a href="update-password.php">Update Password</a></li>
+            <li><a href="my-booking.php">My Booking</a></li>
+            <li><a href="post-testimonial.php">Post a Testimonial</a></li>
+          <li><a href="my-testimonials.php">My Testimonial</a></li>
+            <li><a href="logout.php">Sign Out</a></li>
+            <?php } else { ?>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Profile Settings</a></li>
+              <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Update Password</a></li>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">My Booking</a></li>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Post a Testimonial</a></li>
+          <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">My Testimonial</a></li>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Sign Out</a></li>
+            <?php } ?>
+          </ul>
+            </li>
+          </ul>
+        </div>
+        <div class="header_search">
+          <div id="search_toggle"><i class="fa fa-search" aria-hidden="true"></i></div>
+          <form action="#" method="get" id="header-search-form">
+            <input type="text" placeholder="Search..." class="form-control">
+            <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+          </form>
+        </div>
+      </div>
+      <div class="collapse navbar-collapse" id="navigation">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">Home</a>    </li>
+          	 
+          <li><a href="page.php?type=aboutus">About Us</a></li>
+          <li><a href="car-listing.php">Car Listing</a>
+          <li><a href="page.php?type=faqs">FAQs</a></li>
+          <li><a href="contact-us.php">Contact Us</a></li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Navigation end --> 
+  
+</header>
