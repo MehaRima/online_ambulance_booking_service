cccc2b27372b033d95c1d77820b705dc4d23e261
SAHELI  BASU <26185528+MehaRima@users.noreply.github.com>
2020-07-13 20:51:54 +0530

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-	<div class="copyrights wrapper">
-			Copyright &copy; <?php echo date("Y")?> All Rights Reserved | Designed by <a href= "">meharima.</a>
-		</div>
-	</footer><!--  end footer  -->
-	
-</body>
-</html>
